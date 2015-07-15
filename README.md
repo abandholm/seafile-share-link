ada9dd0a9dcf717b73ae853eb32061ebc598dc96
Anders Bandholm <git@d7.dk>
2015-07-15 17:56:35 +0200

    Nautilus wrapper added
    Also install-script added and README updated.

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # README #
 
-First a script to create share-links from the command line.
+A script to create share-links from the command line plus a 
+wrapper that allows you to use it from Nautilus.
 
-Later a GUI-wrapper to be used from nautilus.
+The Nautilus-functionality requires `nautilus-script-manager` or
+`nautilus-scripts-manager`.
 
-Version 0 :-)
+*Version:* 0 :-)
 
-### How do I get set up? ###
+## How do I get set up? ##
 
-* copy `seafile-share-link` into your PATH (`~/bin` or maybe `/usr/local/bin`)
-* you will need to have `sqlite3` command-line utility installed.
+### Prerequisites ###
+
+`seafile-share-link` requires that `sqlite3` is installed.
+It is used to look into the Seafile-config-files.
+
+`nautilus-seafile-share-link` is meant to be called as "Nautilus Script",
+and requires `nautilus-script-manager` or `nautilus-scripts-manager`.
+
+### Installation ###
+
+    sudo ./install.sh
---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ "$1" ]
+then
+    TARGET="$1"
+elif [ "$PREFIX" ]
+then
+    TARGET="$PREFIX/bin"
+else
+    TARGET="/usr/local/bin"
+fi
+
+# check prereqs
+[ -x `which sqlite3` ] || { "Error: You need 'sqlite3' to run seafile-share-link" >&2; exit 1; }
+[ -d "$TARGET" ] && [ -w "$TARGET" ] || {
+    echo "You do not have write-access to $TARGET -- re-run as root or use sudo" >&2
+    exit 2
+}
+
+FILE="seafile-share-link"
+install --mode=555 "$FILE" "$TARGET" && echo "$FILE installed"
+
+if [ -z "`which nautilus-script-manager`" ] && [ -z "`which nautilus-scripts-manager`" ]
+then
+    echo "The 'nautilus-seafile-share-link' is not installed because it looks like you do" 
+    echo "not have 'nautilus-script-manager' or 'nautilus-scripts-manager' installed"
+    exit
+fi
+
+TARGET="/usr/share/nautilus-scripts"
+[ -d "$TARGET" ] && [ -w "$TARGET" ] || {
+    echo "You do not have write-access to $TARGET -- re-run as root or use sudo" >&2
+    exit 2
+}
+
+FILE='nautilus-seafile-share-link'
+install --mode=555 "$FILE" "$TARGET" && echo "$FILE installed" && {
+    echo "You can now activate the script in Nautilus by using"
+    echo
+    echo "    nautilus-script-manager"
+    echo "or"
+    echo "    nautilus-scripts-manager"
+}
